4f605c5ae6298f1d61e85081ad7bfc93af542e10
shailitrivedi <shailitrivedi@trentu.ca>
2025-06-04 10:44:55 -0400

    formatting files

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -10,16 +10,15 @@ import base.TestBase;
 
 public class HomePage extends TestBase {
 
-	@FindBy(xpath = "//td[contains(text(),'User: Naveen K')]")
+	@FindBy(xpath = "//td[contains(text(),'User: Shai T')]")
 	@CacheLookup
 	WebElement userNameLabel;
 
 	@FindBy(xpath = "//a[contains(text(),'Contacts')]")
 	WebElement contactsLink;
-	
+
 	@FindBy(xpath = "//a[contains(text(),'New Contact')]")
 	WebElement newContactLink;
-	
 
 	@FindBy(xpath = "//a[contains(text(),'Deals')]")
 	WebElement dealsLink;
@@ -31,37 +30,29 @@ public class HomePage extends TestBase {
 	public HomePage() {
 		PageFactory.initElements(driver, this);
 	}
-	
-	//returning Homepage Title
-	public String verifyHomePageTitle(){
+
+	// returning Homepage Title
+	public String verifyHomePageTitle() {
 		return driver.getTitle();
 	}
-	
-	//Returning username as True/false
-	public boolean verifyCorrectUserName(){
+
+	// Returning username as True/false
+	public boolean verifyCorrectUserName() {
 		return userNameLabel.isDisplayed();
 	}
-	
-	//Returning object of ContactsPage after performing click
-	public ContactsPage clickOnContactsLink(){
+
+	// Returning object of ContactsPage after performing click
+	public ContactsPage clickOnContactsLink() {
 		contactsLink.click();
 		return new ContactsPage();
 	}
-	
-	
-	//click on new contact with action class (Hovering of mouse)
-	public void newContactLink(){
+
+	// click on new contact with action class (Hovering of mouse)
+	public void newContactLink() {
 		Actions action = new Actions(driver);
 		action.moveToElement(contactsLink).build().perform();
 		newContactLink.click();
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
 
 }
